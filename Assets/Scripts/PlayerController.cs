df6dce8e4050f8c2922036e56166c42dca1ef051
chriswastyping <76431488+chriswastyping@users.noreply.github.com>
2023-12-07 16:17:03 +0000

    Wheels

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,30 @@
-using System.Collections;
+ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
 
-    [SerializeField] private float speed = 15.0f;
+    [SerializeField] private float horsePower = 0;
+    [SerializeField] float speed = 15.0f;
     private const float turnSpeed = 40.0f;
     private float horizontalInput;
     private float forwardInput;
 
+    private Rigidbody playerRb;
+
+    [SerializeField] GameObject centreOfMass;
+
+    [SerializeField] TextMeshProUGUI speedometerText;
+    [SerializeField] TextMeshProUGUI rpmText;
+    [SerializeField] float rpm;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();    
+        playerRb.centerOfMass = centreOfMass.transform.position;
+    }
 
     // Update is called once per frame
     void FixedUpdate()
@@ -19,9 +34,15 @@ public class PlayerController : MonoBehaviour
         forwardInput = Input.GetAxis("Vertical");
        
         // moves the car forward based on vertical input
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         // Rotates the car based on horizontal input
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
-        
+
+        playerRb.AddRelativeForce(Vector3.forward * horsePower * forwardInput);
+
+        speed = Mathf.RoundToInt(playerRb.velocity.magnitude * 2.237f); // For KPH CHANGE 2.237 TO 3.6
+        speedometerText.SetText("Speed: " + speed + "mph");
+        rpm = Mathf.Round((speed % 30) * 40);
+        rpmText.SetText("RPM: " + rpm);
     }
 }
